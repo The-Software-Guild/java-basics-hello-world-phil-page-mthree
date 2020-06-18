ca68f74a26dcf63a586583467f08b9a814461f46
Phil Page <phil.page@mthree.com>
2020-06-18 11:23:12 +0100

    Signed-off-by: Phil Page <phil.page@mthree.com>

--- helloWorldMaven/src/main/java/mthree/helloworldmaven/Hello.java
+++ helloWorldMaven/src/main/java/mthree/helloworldmaven/Hello.java
@@ -8,5 +8,6 @@ package mthree.helloworldmaven;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello Again!");
     }
 }
